7516b677728f01224f5599dbe71ca5f93a79744e
ashleyoldhamsonos <ashley.oldham@sonos.com>
2022-12-13 12:40:45 +0000

    Initial Commit

---
+++ BLESwiftUI/BLESwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLESwiftUIApp.swift
+//  BLESwiftUI
+//
+//  Created by Ashley Oldham on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BLESwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
